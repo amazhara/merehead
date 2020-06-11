4107a5e4575152fb2c91248abbad5a0b00db0ef3
amazhara <levilater@gmail.com>
2020-06-11 05:05:01 +0300

    Add author seed

--- database/seeds/AuthorSeeder.php
+++ database/seeds/AuthorSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AuthorSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(AuthorSeeder::class);
+        factory(App\Author::class, 10)->create();
     }
 }
